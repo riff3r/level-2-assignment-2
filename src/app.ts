e33ac59990c2761f70e556271f5684be34659917
riff3r <arif4alll@gmail.com>
2023-12-02 21:18:17 +0600

    ts-node-dev & script added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('/demo', (req: Request, res: Response) => {
+  res.send('Demo World!');
 });
 
 export default app;
